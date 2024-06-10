2360d7df21091e26e26476853f4b2f07d9729b85
F. Joe Nichols <joe@fjoenichols.com>
2024-06-09 20:13:09 -0600

    enable cookie security

--- freedome/production_settings.py
+++ freedome/production_settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
