b358b55f107fd4977b8adcd1ac3c01a82eceb4c0
Kyle <79387747+kephs@users.noreply.github.com>
2024-06-15 17:20:44 -0600

    Implemented Task and Subtask (#22)

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Project
 
+from .models import Task, Subtask
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 class ProjectForm(forms.ModelForm):
     title = forms.CharField(max_length=100, required=True)
     description = forms.CharField(widget=forms.Textarea, required=False)
@@ -8,3 +12,37 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description']
+STATUS_CHOICES = ( 
+    ("Backlog", "Backlog"), 
+    ("To Do", "To Do"), 
+    ("In Progress", "In Progress"), 
+    ("In Review", "In Review"), 
+    ("Done", "Done"), 
+    ("Cancelled", "Cancelled"), 
+) 
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False)
+    due_date = forms.DateField(required=False,
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+        input_formats=["%Y-%m-%d"]
+    )
+
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status', 'due_date']
+
+class SubtaskForm(forms.ModelForm):
+    title = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False)
+    due_date = forms.DateField(required=True,
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+        input_formats=["%Y-%m-%d"]
+    )
+
+    class Meta:
+        model = Subtask
+        fields = ['title', 'description', 'status', 'due_date']
