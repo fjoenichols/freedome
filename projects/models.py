94e08cea47bf13ce9bee5a373c8e15a8eec7ed64
Kyle Josephs <kephs@nmsu.edu>
2024-06-15 17:16:43 -0600

    Implemented Task and Subtask

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,25 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    due_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Subtask(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    due_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+class TaskComment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    parent = models.ForeignKey(Task, on_delete=models.CASCADE)
+    message = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
