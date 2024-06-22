385ddc2b88237deba50dc69e36c0df258fa3abb9
F. Joe Nichols <joe@fjoenichols.com>
2024-06-22 10:28:22 -0600

    implement user profile and allow profile edits

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,18 @@
 from django.contrib.auth import login, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django_activitypub.models import LocalActor
-from .forms import RegisterForm
+from accounts.forms import RegisterForm, CustomUserForm
+from projects.models import Project
+from django.conf import settings
 
 def index(request):
-    try:
-        actor = LocalActor.objects.get(name=request.user)
-        context = {
-            "actor": actor,
-        }
-        return render(request, "accounts/index.html", context)
-    except:
+    if (request.user.is_authenticated):
+        return redirect(settings.LOGIN_REDIRECT_URL)
+    else:
         return render(request, "accounts/index.html")
 
 def register(request):
@@ -45,7 +44,7 @@ def create_local_actor(request):
     print('creating local actor for ' + request.user.username)
     user = get_user_model().objects.get(username=request.user)
     LocalActor.objects.create(user=user, name=user.username, preferred_username=user.username, domain="freedome.us")
-    return redirect('index')
+    return redirect('profile')
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
@@ -53,4 +52,39 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('index')
-        
+
+@login_required
+def profile(request):
+    account = request.user
+    projects = Project.objects.filter(owner=account)
+    try:
+        actor = LocalActor.objects.get(name=request.user)
+        context = {
+            "actor": actor,
+            "user": account,
+            "projects": projects,
+        }
+        return render(request, "accounts/profile.html", context)
+    except:
+        context = {
+            "user": account,
+            "projects": projects,
+        }
+        return render(request, "accounts/profile.html", context)
+
+@login_required
+def profile_update(request):
+    account = request.user
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST, request.FILES, instance=account)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.save()
+            return redirect('profile')
+    else: 
+        form = CustomUserForm(instance=account)
+    context = {
+        "user": account,
+        "form": form,
+    }
+    return render(request, 'accounts/profile_update.html', context)
